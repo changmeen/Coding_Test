c01db8b4a5f691bcac0e94a80a5edff78ef53309
rhewjunior <fbckdals99@gmail.com>
2024-04-22 10:14:15 +0900

    24-04-22

---
+++ Baekjoon/silver/No_19947.java
@@ -0,0 +1,30 @@
+package Baekjoon.silver;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class No_19947 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int H = Integer.parseInt(st.nextToken());
+        int Y = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[Y + 1];
+        dp[0] = H;
+
+        for(int i = 1; i <= Y; i++) {
+            dp[i] = (int) (dp[i - 1] * 1.05);
+
+            if(i >= 3) {
+                dp[i] = Math.max(dp[i], (int) (dp[i - 3] * 1.2));
+            }
+            
+            if(i >= 5) {
+                dp[i] = Math.max(dp[i], (int) (dp[i - 5] * 1.35));
+            }
+        }
+
+        System.out.println(dp[Y]);
+    }
+}
